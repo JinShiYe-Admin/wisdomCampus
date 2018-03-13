6b2b8d90ee88560ed8e9d613f43e07842baf6854
Zqw <578159259@qq.com>
2018-03-13 08:40:58 +0800

    更新

--- wisdomCampus/js/utils/storageKeyName.js
+++ wisdomCampus/js/utils/storageKeyName.js
@@ -2,7 +2,7 @@
 
 var storageKeyName = (function(mod) {
 
-	mod.key = 0; 
+	mod.key = 1; 
 	var exLog = console.log;
 	console.log = function(hint, object) {
 		if(mod.key === 0) {
@@ -20,13 +20,12 @@ var storageKeyName = (function(mod) {
 			mod.INTERFACEGU = 'https://jsypay.jiaobaowang.net/useradminwebapi/api/data/';//顾工接口
 			mod.INTERFACEKONG = 'https://jbyj.jiaobaowang.net/SchoolOAService/';//孔工接口
 			break;
-		case 0: //发布
-			mod.SCHOOLID = 100005;//学校ID
+		case 1: //发布
+			mod.SCHOOLID = 100008;//学校ID
 			mod.USERTYPE = 0;//用户类型，0老师,1家长,2学生
-			mod.INTERFACEGU = 'https://jsypay.jiaobaowang.net/useradminwebapi/api/data/';//顾工接口
-			mod.INTERFACEKONG = 'https://jbyj.jiaobaowang.net/SchoolOAService/';//孔工接口
+			mod.INTERFACEGU = 'https://zhxyapi.jiaobaowang.net:8501/api/Data/';//顾工接口
+			mod.INTERFACEKONG = 'https://jbyj.jiaobaowang.net:8443/SchoolOAService/';//孔工接口
 			break;
-
 		default:
 			break;
 	}
