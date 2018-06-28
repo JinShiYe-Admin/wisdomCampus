ea33c1562d19b2c457a13958a57e68357878c894
Zqw <>
2018-06-28 11:20:38 +0800

    修改密码，头像

--- wisdomCampus/js/lib/app-update.js
+++ wisdomCampus/js/lib/app-update.js
@@ -147,8 +147,8 @@ var appUpdate = (function(mod) {
 				}
 			}
 		} else { //ios
-			if(version) {
-				newestVersions = version.version.split('.');
+			if(versionInfo) {
+				newestVersions = versionInfo.version.split('.');
 				var hasNewerVersion = newestVersions.some(function(verNo, index) {
 					return parseInt(verNo) > parseInt(appVersions[index]);
 				})
--- wisdomCampus/js/utils/MultiMedia.js
+++ wisdomCampus/js/utils/MultiMedia.js
@@ -41,7 +41,7 @@ var MultiMedia = (function($, mod) {
 		//初始化
 		this.init();
 		this.initData();
-		this.initEvent();
+//		this.initEvent();
 	}
 
 	var proto = MultiMedia.prototype; //属性使您有能力向对象添加属性和方法。
